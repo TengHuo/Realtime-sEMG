5a45e387844d9c9b33cc91e99c3bf163880ed997
TENG HUO <teng_huo@outlook.com>
2019-03-26 22:26:11 +0000

    rewrite capg data loader

--- emg/utils/data.py
+++ emg/utils/data.py
@@ -1,10 +1,22 @@
+# -*- coding: UTF-8 -*-
+# data.py
+# @Time     : 22/Mar/2019
+# @Auther   : TENG HUO
+# @Email    : teng_huo@outlook.com
+# @Version  : 1.0.0
+# @License  : MIT
+#
+#
 import numpy as np
 import os
 from scipy.io import loadmat
 import re
 from enum import Enum, unique
+import h5py
 
-# TODO: 1. implement a csl data loader
+# TODO: 1. 添加dbb和dbc的load代码，封装不同格式数据处理的代码
+# TODO: 2. 添加测试
+# TODO: 2. implement a csl data loader
 # TODO: 2. merge capg and csl data loader, train models with all data
 # TODO: 3. implement a data generator for generating triplet pairs
 
@@ -19,35 +31,94 @@ class CapgDBName(Enum):
     
 LoadMode = Enum('LoadMode', ('flat', 'flat_frame', 'sequence', 'sequence_frame'))
 
-def load_capg_data(db_name, mode=LoadMode.sequence):
+
+class CapgData(object):
+    pass
+
+def load_capg_all(mode=LoadMode.sequence):
+    # x =（20, None, 128)
+    # y = (20, None, 1)
+    # 一次行读取全部capg数据
+    
+    dba = _load_capg_data(CapgDBName.dba)
+    dbb = _load_capg_data(CapgDBName.dbb)
+    dbc = _load_capg_data(CapgDBName.dbc)
+
+    capg = {gesture_index: None for gesture_index in range(20)}
+    for i in dba.keys():
+        capg_index = i - 1
+        capg[capg_index] = np.asarray(dba[i] + dbb[i])
+
+    for i in dbc.keys():
+        capg_index = i + 7
+        capg[capg_index] = np.asarray(dbc[i])
+
+    for i in capg.keys():
+        original_shape = capg[i].shape
+        if mode == LoadMode.sequence:
+            # default shape (None, 1000, 128): sequence mode
+            continue
+        elif mode == LoadMode.sequence_frame:
+            capg[i] = capg[i].reshape(original_shape[0], 1000, 16, 8, 1)
+        elif mode == LoadMode.flat:
+            # reshape to (None, 128)
+            capg[i] = capg[i].reshape(original_shape[0]*1000, 128)
+        elif mode == LoadMode.flat_frame:
+            # reshape to (None, 16, 8)
+            capg[i] = capg[i].reshape(original_shape[0]*1000, 16, 8, 1)
+        # print(capg[i].shape)
+    return capg
+
+def capg_split_train_test(data, test_size=0.2, random_state=None):
+    train = {gesture_index: None for gesture_index in range(20)}
+    test = {gesture_index: None for gesture_index in range(20)}
+
+    for i in data.keys():
+        shape = data[i].shape
+        test_index = np.random.choice(shape[0], int(shape[0]*test_size))
+        test[i] = data[i][test_index]
+        train[i] = np.delete(data[i], test_index, axis=0)
+        # print(train[i].shape)
+        # print(test[i].shape)
+        # print()
+
+    return train, test
+
+def prepare_data(data, required_gestures=8):
+    required_range = range(required_gestures)
+    X = list()
+    y = list()
+    for i in required_range:
+        amount = data[i].shape[0]
+        X.append(data[i])
+        y += [i for _ in range(amount)]
+    X = np.concatenate(X, axis=0)
+    y = np.asarray(y)
+    return X, y
+
+def load_capg_from_h5(file_name):
+    with h5py.File(file_name, 'r') as data_file:
+        train = np.array(data_file['train'])
+        test = np.array(data_file['test'])
+    return train, test
+
+def save_capg_to_h5(train, test, file_name):
+    with h5py.File(file_name, 'w') as data_file:
+        data_file.create_dataset('train', data=train, dtype=np.float)
+        data_file.create_dataset('test', data=test, dtype=np.float)
+        
+def _load_capg_data(db_name):
     # get the parent dir path
     now_path = os.path.join(os.sep, *os.path.dirname(os.path.realpath(__file__)).split(os.sep)[:-2])
     data_path = os.path.join(now_path, 'data', 'capg', db_name.value)
     mat_list = os.listdir(data_path)
-    x, y, category = _read_capg_mat_files(data_path, mat_list)
+    emg_data = _read_capg_mat_files(data_path, mat_list)
     
-    if mode == LoadMode.sequence:
-        # default shape (None, 1000, 128): sequence mode
-        pass
-    elif mode == LoadMode.sequence_frame:
-        x = x.reshape(x.shape[0], x.shape[1], 16, 8, 1)
-    elif mode == LoadMode.flat:
-        # reshape to (None, 128)
-        x = x.reshape(x.shape[0]*1000, 128)
-        y = y.reshape(y.shape[0], 1) * np.ones((1, 1000))
-        y = y.flatten()
-    elif mode == LoadMode.flat_frame:
-        # reshape to (None, 16, 8)
-        x = x.reshape(x.shape[0]*1000, 16, 8, 1)
-        y = y.reshape(y.shape[0], 1) * np.ones((1, 1000))
-        y = y.flatten()
-
-    return x, y, category
+    return emg_data
 
 def _read_capg_mat_files(path, mat_list):
-    x = list()
-    y = list()
-    gesture_set = set()
+    data = dict()
+
     for mat_name in mat_list:
         if not re.match(r'\S*[.]mat$', mat_name, flags=0):
             continue
@@ -56,12 +127,12 @@ def _read_capg_mat_files(path, mat_list):
         gesture_index = mat['gesture'][0][0]
         if gesture_index in [100, 101]:
             continue
-        else:
-            gesture_set.add(gesture_index)
         
-        x.append(mat['data'])
-        y.append(gesture_index)
-    return np.asarray(x), np.asarray(y), len(gesture_set)
+        if gesture_index not in data.keys():
+            data[gesture_index] = list()
+        data[gesture_index].append(mat['data'])
+
+    return data
 
 
 if __name__ == '__main__':
@@ -71,19 +142,19 @@ if __name__ == '__main__':
         'dbb': (1600, 1000, 128),
         'dbc': (1200, 1000, 128)
     }
-    for db_name in seq_data_dim.keys():
-        x, y, c = load_capg_data(db_name=CapgDBName[db_name])
-        assert x.shape == seq_data_dim[db_name]
+    # for db_name in seq_data_dim.keys():
+    #     x, y, c = load_capg_data(db_name=CapgDBName[db_name])
+    #     assert x.shape == seq_data_dim[db_name]
 
-    flat_data_dim = {
-        'dba': (1440000, 128),
-        'dbb': (1600000, 128),
-        'dbc': (1200000, 128)
-    }
-    for db_name in flat_data_dim.keys():
-        x, y, c = load_capg_data(db_name=CapgDBName[db_name], mode=LoadMode.flat)
-        print(x.shape)
-        assert x.shape == flat_data_dim[db_name]
+    # flat_data_dim = {
+    #     'dba': (1440000, 128),
+    #     'dbb': (1600000, 128),
+    #     'dbc': (1200000, 128)
+    # }
+    # for db_name in flat_data_dim.keys():
+    #     x, y, c = load_capg_data(db_name=CapgDBName[db_name], mode=LoadMode.flat)
+    #     print(x.shape)
+    #     assert x.shape == flat_data_dim[db_name]
 
 
 
