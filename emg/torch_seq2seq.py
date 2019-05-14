47fa028ce036b5d8eab2deb81bdd339e772ff486
TENG HUO <teng_huo@outlook.com>
2019-05-14 17:04:40 +0100

    fix bug in seq2seq2 model: can't train with sequence result

--- emg/torch_seq2seq.py
+++ emg/torch_seq2seq.py
@@ -1,23 +1,10 @@
 # -*- coding: UTF-8 -*-
 
 
-from io import open
-import unicodedata
-import string
-import re
-import random
-
 import torch
 import torch.nn as nn
 from torch import optim
 from torch.utils.data import DataLoader
-
-import torch.nn.functional as F
-
-from ignite.engine import Events, Engine, create_supervised_evaluator
-from ignite.utils import convert_tensor
-from ignite.contrib.handlers.param_scheduler import LRScheduler
-from ignite.metrics import Accuracy, Loss
 
 from emg.utils import CapgDataset
 
@@ -26,11 +13,11 @@ class EncoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(EncoderRNN, self).__init__()
 
-        self.rnn = nn.GRU(         # if use nn.RNN(), it hardly learns
+        self.rnn = nn.GRU(
             input_size=input_size,
-            hidden_size=hidden_size,         # rnn hidden unit
-            num_layers=1,           # number of rrnn layer
-            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+            hidden_size=hidden_size,
+            num_layers=1,
+            batch_first=True,
         )
 
     def forward(self, x):
@@ -42,24 +29,24 @@ class DecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size):
         super(DecoderRNN, self).__init__()
 
-        self.rnn = nn.GRU(         # if use nn.RNN(), it hardly learns
+        self.rnn = nn.GRU(
             input_size=hidden_size,
-            hidden_size=hidden_size,         # rnn hidden unit
-            num_layers=1,           # number of rnn layer
-            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+            hidden_size=hidden_size,
+            num_layers=1,
+            batch_first=True,
         )
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x, prev_hidden):
-        output, _ = self.rnn(x, prev_hidden)
-        return self.fc(output[:, -1, :])
+        output, hidden = self.rnn(x, prev_hidden)
+        return output, self.fc(output[:, 0, :]), hidden
 
 
 def get_data_loaders(gesture_num, train_batch_size, val_batch_size, sequence_len):
-    train_loader = DataLoader(CapgDataset(gestures=gesture_num, sequence_len=sequence_len, sequence_result=False,
+    train_loader = DataLoader(CapgDataset(gestures=gesture_num, sequence_len=sequence_len, sequence_result=True,
                                           train=True), batch_size=train_batch_size, shuffle=True)
 
-    val_loader = DataLoader(CapgDataset(gestures=gesture_num, sequence_len=sequence_len, sequence_result=False,
+    val_loader = DataLoader(CapgDataset(gestures=gesture_num, sequence_len=sequence_len, sequence_result=True,
                                         train=False), batch_size=val_batch_size, shuffle=False)
 
     return train_loader, val_loader
@@ -90,37 +77,30 @@ if __name__ == "__main__":
 
     for e in range(epoch):  # loop over the dataset multiple times
 
-        loss100 = 0.0
+        avg_loss = 0.0
         for i, data in enumerate(train_loader, 0):
             # get the inputs
             x, labels = data
 
-            encoder_output, hidden = encoder(x)
-            pred = decoder(encoder_output, hidden)
-            # print(pred.size())
-
             encoder_optimizer.zero_grad()
             decoder_optimizer.zero_grad()
 
-            # test1 = pred_seq[:, 0, :]
-            # test2 = labels[:, 0]
-            # print(pred_seq[:, 0, :].size())
-            # print(labels[:, 0].size())
-            loss = criterion(pred, labels)
-
-            # loss = 0.0
-            # for i in range(pred_seq.size(1)):
-            #     loss += criterion(pred_seq[:, i, :], labels[:, i])
-
+            _, hidden = encoder(x)
+            decoder_input = torch.zeros((train_batch_size, 1, hidden_size), device=device)
+            loss = 0.0
+            for i in range(seq_length):
+                decoder_input, pred, hidden = decoder(decoder_input, hidden)
+                loss += criterion(pred, labels[:, i])
             # print(loss)
+
             loss.backward()
             encoder_optimizer.step()
             decoder_optimizer.step()
 
-            loss100 += loss.item()
+            avg_loss += loss.item()
             if i % 100 == 99:
                 print('[Epoch %d, Batch %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, loss100 / 100))
-                loss100 = 0.0
+                      (epoch, i + 1, avg_loss / 100))
+                avg_loss = 0.0
 print("Done Training!")
 
