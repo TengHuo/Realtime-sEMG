2529133ac040e3f70a56138b845ad945c659599f
TENG HUO <teng_huo@outlook.com>
2019-05-24 23:58:17 +0100

    add new feature: reshape x to frame format

--- emg/utils/capg_data.py
+++ emg/utils/capg_data.py
@@ -9,14 +9,12 @@
 #
 
 import os
-import pickle
 import re
 import h5py
 
-import torch
 from torch.utils import data
-import numpy as np
 from scipy.io import loadmat
+import numpy as np
 
 
 class CapgDataset(data.Dataset):
@@ -27,7 +25,9 @@ class CapgDataset(data.Dataset):
     supporting integer indexing in range from 0 to len(self) exclusive.
     """
 
-    def __init__(self, gestures=8, sequence_len=1, sequence_result=False, train=True, transform=None):
+    def __init__(self, gesture=8, sequence_len=1, sequence_result=False,
+                 frame_x=False, train=True, transform=None):
+
         self.transform = transform
         self.train = train  # training set or test set
 
@@ -36,16 +36,16 @@ class CapgDataset(data.Dataset):
         if not os.path.isfile(data_file_path):
             print('data not exist, create a new h5 file')
             capg_data = _load_capg_all()
-            train, test = _capg_train_test_split(capg_data, test_size=0.1)
-            _save_capg_to_h5(train, test, data_file_path)
+            train_data, test_data = _capg_train_test_split(capg_data, test_size=0.1)
+            _save_capg_to_h5(train_data, test_data, data_file_path)
         else:
             print('data exist, load {} data from the file'.format('train' if train else 'test'))
-            train, test = _load_capg_from_h5(data_file_path)
+            train_data, test_data = _load_capg_from_h5(data_file_path)
 
         if self.train:
-            X, y = _prepare_data(train, required_gestures=gestures)
+            X, y = _prepare_data(train_data, gesture_num=gesture)
         else:
-            X, y = _prepare_data(test, required_gestures=gestures)
+            X, y = _prepare_data(test_data, gesture_num=gesture)
 
         original_shape = X.shape
         if 1 == sequence_len:
@@ -56,12 +56,18 @@ class CapgDataset(data.Dataset):
         else:
             # reshape to (None, seq_len, 128)
             seq_amount = int(1000/sequence_len)
-            X = X.reshape(original_shape[0]*seq_amount, sequence_len, 128)
+            X = X.reshape((original_shape[0]*seq_amount, sequence_len, 128))
             y = y.reshape((y.shape[0], 1)) * np.ones((1, seq_amount))
             y = y.flatten()
 
         if sequence_result:
             y = y.reshape((y.shape[0], 1)) * np.ones((1, sequence_len))
+
+        # reshape x to frame (16, 8)
+        if frame_x:
+            old_shape = X.shape
+            new_shape = (*old_shape[0:-1], 16, 8)
+            X = X.reshape(new_shape)
 
         y = y.astype(int)
         # self.data, self.targets = X[:2048], y[:2048]  # for test
@@ -86,15 +92,15 @@ class CapgDataset(data.Dataset):
         return len(self.data)
 
 
-def _prepare_data(data, required_gestures=8):
-    """split train and test dataset
+def _prepare_data(raw_data, gesture_num=8):
+    """convert the data from raw data to numpy array
     """
-    required_range = range(required_gestures)
+    required_range = range(gesture_num)
     X = list()
     y = list()
     for i in required_range:
-        amount = data[i].shape[0]
-        X.append(data[i])
+        amount = raw_data[i].shape[0]
+        X.append(raw_data[i])
         y += [i for _ in range(amount)]
     X = np.concatenate(X, axis=0)
     X = X.astype(np.float32)
@@ -121,45 +127,45 @@ def _load_capg_all():
     return capg
 
 
-def _capg_train_test_split(data, test_size=0.1):
-    train = dict()
-    test = dict()
+def _capg_train_test_split(raw_data, test_size=0.1):
+    train_set = dict()
+    test_set = dict()
 
-    for i in data.keys():
-        shape = data[i].shape
+    for i in raw_data.keys():
+        shape = raw_data[i].shape
         test_index = np.random.choice(shape[0], int(shape[0]*test_size), replace=False)
         train_index = np.delete(np.arange(shape[0]), test_index)
-        test[i] = data[i][test_index]
-        train[i] = data[i][train_index]
+        test_set[i] = raw_data[i][test_index]
+        train_set[i] = raw_data[i][train_index]
 
-    return train, test
+    return train_set, test_set
 
 
 def _load_capg_from_h5(file_name):
     with h5py.File(file_name, 'r') as data_file:
-        train = dict()
+        train_set = dict()
         train_grp = data_file['train']
         print()
         for i in train_grp.keys():
-            train[int(i)] = train_grp[i].value
+            train_set[int(i)] = train_grp[i][()]
 
-        test = dict()
+        test_set = dict()
         test_grp = data_file['test']
         for i in test_grp.keys():
-            test[int(i)] = test_grp[i].value
+            test_set[int(i)] = test_grp[i][()]
 
-    return train, test
+    return train_set, test_set
 
 
-def _save_capg_to_h5(train, test, file_name):
+def _save_capg_to_h5(train_data, test_data, file_name):
     with h5py.File(file_name, 'w') as data_file:
         train_grp = data_file.create_group('train')
-        for gesture in train.keys():
-            train_grp.create_dataset(str(gesture), data=train[gesture], dtype=float)
+        for gesture in train_data.keys():
+            train_grp.create_dataset(str(gesture), data=train_data[gesture], dtype=float)
 
         test_grp = data_file.create_group('test')
-        for gesture in test.keys():
-            test_grp.create_dataset(str(gesture), data=test[gesture], dtype=float)
+        for gesture in test_data.keys():
+            test_grp.create_dataset(str(gesture), data=train_data[gesture], dtype=float)
 
 
 def _load_capg_data(db_name):
@@ -173,7 +179,7 @@ def _load_capg_data(db_name):
 
 
 def _read_capg_mat_files(path, mat_list):
-    data = dict()
+    mat_data = dict()
 
     for mat_name in mat_list:
         if not re.match(r'\S*[.]mat$', mat_name, flags=0):
@@ -184,18 +190,16 @@ def _read_capg_mat_files(path, mat_list):
         if gesture_index in [100, 101]:
             continue
         
-        if gesture_index not in data.keys():
+        if gesture_index not in mat_data.keys():
             data[gesture_index] = list()
-        data[gesture_index].append(mat['data'])
+        mat_data[gesture_index].append(mat['data'])
 
-    return data
-
-
-# def load_history(file_path):
-#     with open(file_path, 'r') as history_file:
-#         return pickle.load(history_file)
+    return mat_data
 
 
-# def save_history(history, path):
-#     with open(path, 'wb') as history_file:
-#         pickle.dump(history, history_file)
+if __name__ == '__main__':
+    # test pytorch data loader
+    test = CapgDataset(gesture=8, sequence_len=10, sequence_result=False,
+                       frame_x=False, train=True, transform=None)
+    print(test.data.shape)
+    print(test.targets.shape)
