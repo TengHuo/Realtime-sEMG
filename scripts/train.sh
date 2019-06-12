f24c88b512e59f9328257b4012752bb8b6560caa
TENG HUO <teng_huo@outlook.com>
2019-06-12 14:53:37 +0100

    add sgd with momentum

--- scripts/train.sh
+++ scripts/train.sh
@@ -13,6 +13,18 @@
 #                      --log_interval 100
 #done
 
+python -m emg.train --model lstm \
+                    --suffix sgd \
+                    --sub_folder momentum-0.9 \
+                    --gesture_num 8 \
+                    --lr 0.01 \
+                    --epoch 200 \
+                    --train_batch_size 256 \
+                    --valid_batch_size 1024 \
+                    --stop_patience 12 \
+                    --log_interval 100
+
+
 # lstm model
 #for((i=8; i <= 20; i++));
 #do
@@ -53,49 +65,49 @@
 #                      --log_interval 100
 #done
 
-python -m emg.train --model mlp \
-                  --suffix compare-gesture \
-                  --sub_folder gesture-8 \
-                  --gesture_num 8 \
-                  --lr 0.001 \
-                  --epoch 200 \
-                  --train_batch_size 256 \
-                  --valid_batch_size 1024 \
-                  --stop_patience 12 \
-                  --log_interval 100
-
-python -m emg.train --model mlp \
-                  --suffix compare-gesture \
-                  --sub_folder gesture-12 \
-                  --gesture_num 12 \
-                  --lr 0.001 \
-                  --epoch 200 \
-                  --train_batch_size 256 \
-                  --valid_batch_size 1024 \
-                  --stop_patience 12 \
-                  --log_interval 100
-
-python -m emg.train --model mlp \
-                  --suffix compare-gesture \
-                  --sub_folder gesture-16 \
-                  --gesture_num 16 \
-                  --lr 0.001 \
-                  --epoch 200 \
-                  --train_batch_size 256 \
-                  --valid_batch_size 1024 \
-                  --stop_patience 12 \
-                  --log_interval 100
-
-python -m emg.train --model mlp \
-                  --suffix compare-gesture \
-                  --sub_folder gesture-20 \
-                  --gesture_num 20 \
-                  --lr 0.001 \
-                  --epoch 200 \
-                  --train_batch_size 256 \
-                  --valid_batch_size 1024 \
-                  --stop_patience 12 \
-                  --log_interval 100
+#python -m emg.train --model mlp \
+#                  --suffix compare-gesture \
+#                  --sub_folder gesture-8 \
+#                  --gesture_num 8 \
+#                  --lr 0.001 \
+#                  --epoch 200 \
+#                  --train_batch_size 256 \
+#                  --valid_batch_size 1024 \
+#                  --stop_patience 12 \
+#                  --log_interval 100
+#
+#python -m emg.train --model mlp \
+#                  --suffix compare-gesture \
+#                  --sub_folder gesture-12 \
+#                  --gesture_num 12 \
+#                  --lr 0.001 \
+#                  --epoch 200 \
+#                  --train_batch_size 256 \
+#                  --valid_batch_size 1024 \
+#                  --stop_patience 12 \
+#                  --log_interval 100
+#
+#python -m emg.train --model mlp \
+#                  --suffix compare-gesture \
+#                  --sub_folder gesture-16 \
+#                  --gesture_num 16 \
+#                  --lr 0.001 \
+#                  --epoch 200 \
+#                  --train_batch_size 256 \
+#                  --valid_batch_size 1024 \
+#                  --stop_patience 12 \
+#                  --log_interval 100
+#
+#python -m emg.train --model mlp \
+#                  --suffix compare-gesture \
+#                  --sub_folder gesture-20 \
+#                  --gesture_num 20 \
+#                  --lr 0.001 \
+#                  --epoch 200 \
+#                  --train_batch_size 256 \
+#                  --valid_batch_size 1024 \
+#                  --stop_patience 12 \
+#                  --log_interval 100
 
 # c3d model
 #python -m emg.app --model c3d \
